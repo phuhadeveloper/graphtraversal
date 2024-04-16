da9b97e4ab689d95a0f9b1f7094237187357a3a0
Phu Ha <phuha.engineer@gmail.com>
2024-04-16 14:03:26 -0400

    added data file

--- graphtraversal/src/App.js
+++ graphtraversal/src/App.js
@@ -1,10 +1,67 @@
 import './App.css';
+import React, {Fragment, useEffect, useState} from 'react';
+import cytoscape from 'cytoscape';
 
 function App() {
+
+  const [jsonData, setJsonData] = useState({});
+
+  const drawgraph = () => {
+    const cy = cytoscape({
+      container: document.getElementById('cy'),
+      elements: jsonData,
+
+      style: [ // the stylesheet for the graph
+        {
+          selector: 'node',
+          style: {
+            'background-color': '#666',
+            'label': 'data(id)'
+          }
+        },
+
+        {
+          selector: 'edge',
+          style: {
+            'width': 3,
+            'line-color': '#ccc',
+            // 'target-arrow-color': '#ccc',
+            // 'target-arrow-shape': 'triangle',
+            'curve-style': 'bezier',
+            //'arrow-scale': 0
+
+          }
+        }
+      ],
+
+      layout: {
+        name: "preset"
+      }
+    });
+  }
+
+  useEffect(() => {
+
+    fetch("./data.json", {"headers": {
+      "Accept": "application/json",
+      'Content-Type': 'application/json'
+    }})
+      .then(res => res.text())
+      .then(data => {
+        console.log(data);
+        setJsonData(data);
+      })
+      .catch(error => console.log(error))
+    
+    drawgraph()
+  }, [])
+
   return (
-    <div>
-      Hello World!
-    </div>
+    <Fragment>
+      <div id='cy' style={{width: '100%', height: '80vh'}}>
+
+      </div>
+    </Fragment>
   );
 }
 
